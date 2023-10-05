6bdbeea56ec898c716314ba654009bf177571031
Justin Simon <jls5177@gmail.com>
2023-10-05 20:47:00 +0000

    Test

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -10,6 +10,9 @@ import edu.wpi.first.wpilibj.RobotBase;
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
  * you are doing, do not modify this file except to change the parameter class to the startRobot
  * call.
+ * 
+ * This is a test of the emergency broadcast system.....x
+ * 
  */
 public final class Main {
   private Main() {}
